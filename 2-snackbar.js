e32cae656904c5e44a34b0869a5491039e6b8175
sofi-dobriak <167961175+sofi-dobriak@users.noreply.github.com>
2025-02-04 13:55:50 +0000

    Deploying to gh-pages from @ sofi-dobriak/goit-js-hw-10@d6eaf2f8fbff5ddc3209a74977bca0678e02ff18 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t={delayInput:document.querySelector('input[name="delay"]'),submitBth:document.querySelector("button")};t.delayInput.classList.add("delay-input");t.submitBth.classList.add("delay-button");
+//# sourceMappingURL=2-snackbar.js.map
