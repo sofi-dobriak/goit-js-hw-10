d6eaf2f8fbff5ddc3209a74977bca0678e02ff18
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-02-04 15:55:32 +0200

    Add markup and styles

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,9 @@
+const refs = {
+  delayInput: document.querySelector('input[name="delay"]'),
+  submitBth: document.querySelector('button'),
+};
+
+// ================== STYLES ==================
+refs.delayInput.classList.add('delay-input');
+refs.submitBth.classList.add('delay-button');
+// =============================================
