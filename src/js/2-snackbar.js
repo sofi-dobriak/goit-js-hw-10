0c68d2301528ef78429abaef1a7b09145c1d35b0
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-02-07 13:11:38 +0200

    Task #2 is done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,9 +1,55 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const refs = {
-  delayInput: document.querySelector('input[name="delay"]'),
-  submitBth: document.querySelector('button'),
+  form: document.querySelector('.form'),
 };
 
-// ================== STYLES ==================
-refs.delayInput.classList.add('delay-input');
-refs.submitBth.classList.add('delay-button');
-// =============================================
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delayInput = refs.form.querySelector('input[name="delay"]').value;
+  const stateInput = refs.form.querySelector(
+    'input[name="state"]:checked'
+  )?.value;
+
+  if (!stateInput) {
+    return;
+  }
+
+  createPromise(stateInput, delayInput)
+    .then(delay => onFulfilled(delay))
+    .catch(delay => onRejected(delay));
+
+  refs.form.reset();
+});
+
+function createPromise(state, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, +delay);
+  });
+
+  return promise;
+}
+
+function onFulfilled(delay) {
+  iziToast.success({
+    title: 'Success',
+    message: `✅ Fulfilled promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
+
+function onRejected(delay) {
+  iziToast.error({
+    title: 'Error',
+    message: `❌ Rejected promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
